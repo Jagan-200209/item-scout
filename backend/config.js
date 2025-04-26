e40d85f398ad8764521bf4538de93dc7dad41ec6
kpremkiran <premkiran323@gmail.com>
2025-04-26 13:54:20 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 8000;
 
-export const mongoURL ="mongodb+srv://jagan_2002:Mongodb2002@cluster0.j9txgxa.mongodb.net/creativity?retryWrites=true&w=majority&appName=Cluster0";
+export const mongoURL ="mongodb+srv://root:SNzJheXm5ObQeQrh@lostandfound.toc33hc.mongodb.net/lost-items?retryWrites=true&w=majority";
